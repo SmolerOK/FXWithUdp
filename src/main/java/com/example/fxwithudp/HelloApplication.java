764d564d753b1356da48ae69cd73e3634d79e306
Сергей Лобанов <sergei.smoler@mail.ru>
2022-09-20 14:03:05 +0300

    Added logging

--- src/main/java/com/example/fxwithudp/HelloApplication.java
+++ src/main/java/com/example/fxwithudp/HelloApplication.java
@@ -4,28 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.*;
-import java.lang.Object.*;
-import java.lang.module.Configuration;
-
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HelloApplication extends Application {
 
     private static final Logger LOGGER = LogManager.getLogger(HelloApplication.class);
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Artsec Marvel");
+            stage.setScene(scene);
+            stage.show();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Artsec Marvel");
-        stage.setScene(scene);
-        stage.show();
+
+            stage.setOnCloseRequest(windowEvent -> {
+                LOGGER.info("Закрытие приложения.");
+            });
+        } catch (Exception ex) {
+            LOGGER.error(ex);
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        try {
+            LOGGER.info("Открытие приложения.");
+            launch();
+        } catch (Exception ex) {
+            LOGGER.error(ex);
+        }
     }
 }
--- src/main/java/com/example/fxwithudp/HelloController.java
+++ src/main/java/com/example/fxwithudp/HelloController.java
@@ -3,16 +3,19 @@ package com.example.fxwithudp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.util.Arrays;
 
 public class HelloController {
 
-
+    private static final Logger LOGGER = LogManager.getLogger(HelloController.class);
     private static final int SERVICE_PORT = 8192;
     private static final byte[] MESSAGE = {0x02, 0x1F, 0x00, 0x03, 0x61, 0x38};
     public Label label1;
@@ -20,10 +23,12 @@ public class HelloController {
 
     @FXML
     public void button0() {
+        LOGGER.info("Нажата кнопка opendoor =0.");
         // Создаем UDP сокет
+        LOGGER.info("Создание сокета.");
         try (MulticastSocket socket = new MulticastSocket()) {
-
             InetAddress ipAddress = InetAddress.getByName(inputIP.getText());
+            LOGGER.info("Чтение ip: " + ipAddress + ":" + SERVICE_PORT);
             InetSocketAddress mainAddress = new InetSocketAddress(ipAddress, SERVICE_PORT);
             socket.setSoTimeout(1000);
 
@@ -31,35 +36,50 @@ public class HelloController {
             DatagramPacket messagePacket = new DatagramPacket(MESSAGE, MESSAGE.length, mainAddress);
 
             // Отправляем сообщение
+            LOGGER.info("Отправка данных: " + Arrays.toString(MESSAGE) + "." + " От: " + InetAddress.getLocalHost());
             socket.send(messagePacket);
 
-            byte[] receiveBuffer = new byte[256];
+            byte[] receiveBuffer = new byte[1024];
 
             // Пакет для получения сообщения
             DatagramPacket outputPacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
             socket.receive(outputPacket);
-
             int packetLength = outputPacket.getLength();
+
+            LOGGER.info("Проверка на объем пришедшего пакета. Получено: " +
+                    packetLength +
+                    " byte. Ответ: " +
+                    convertByteToHex(receiveBuffer, packetLength) +
+                    "." +
+                    " От " +
+                    outputPacket.getAddress() +
+                    ":" +
+                    outputPacket.getPort());
+
+            if (packetLength > 256)
+                throw new RuntimeException("Полученные данные превышают размер буфера. Размер " + packetLength + " byte.");
 
             //Получаем сообщение
             label1.setText("Message: " + convertByteToHex(receiveBuffer, packetLength));
+            LOGGER.info("Закрытие сокета.");
 
         } catch (IOException e) {
+            LOGGER.error("Ошибка: " + e);
             label1.setText(e.getMessage());
         }
     }
 
     // Перевод byte в hex
     public static String convertByteToHex(byte[] arraysMessage, int packetL) {
-        if(packetL > 256)
-            throw new RuntimeException("Buffer overflowing");
-
         StringBuilder hex = new StringBuilder();
-
-        for (int i = 0; i < packetL; i++) {
-            hex.append(String.format("%02X", arraysMessage[i]));
+        try {
+            LOGGER.info("Конвертация BYTE в HEX.");
+            for (int i = 0; i < packetL; i++) {
+                hex.append(String.format("%02X", arraysMessage[i]));
+            }
+        } catch (Exception ex) {
+            LOGGER.error(ex);
         }
-
         return hex.toString();
     }
 
