8bee25e7a9a945a83e05487e5967391ffe59ee57
Сергей Лобанов <sergei.smoler@mail.ru>
2022-09-20 10:40:25 +0300

    Add dependencies, xml and class log4j2

--- src/main/java/com/example/fxwithudp/HelloApplication.java
+++ src/main/java/com/example/fxwithudp/HelloApplication.java
@@ -5,11 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.Object.*;
+import java.lang.module.Configuration;
+
+import org.apache.logging.log4j.*;
 
 public class HelloApplication extends Application {
+
+    private static final Logger LOGGER = LogManager.getLogger(HelloApplication.class);
+
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Artsec Marvel");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.fxwithudp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
 
     opens com.example.fxwithudp to javafx.fxml;
